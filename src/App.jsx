c1a77ce4f82c71cedbbf83e6af4322a37df89641
MarioNaise <eharmuth@carhartt-wip.com>
2023-06-02 18:26:27 +0200

    Turn App.jsx into function component

--- src/App.jsx
+++ src/App.jsx
@@ -1,124 +1,137 @@
-import React from 'react'
-import './App.css'
-import BreakControl from "./BreakControl"
-import SessionControl from "./SessionControl"
-import Session from "./Session"
-import Controls from "./Controls"
+import React, { useState } from "react";
+import "./App.css";
+import BreakControl from "./BreakControl";
+import SessionControl from "./SessionControl";
+import Session from "./Session";
+import Controls from "./Controls";
 
-export default class App extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      breakTimeDisplay: 5,
-      sessionTime: 1500,
-      sessionTimeDisplay: 25,
-      counting: false,
-      timerLabel: "Session"
+export default function App() {
+  const [breakTimeDisplay, setBreakTimeDisplay] = useState(5);
+  const [sessionTimeDisplay, setSessionTimeDisplay] = useState(25);
+  const [counting, setCounting] = useState(false);
+  const [counter, setCounter] = useState(null);
+  const [sessionData, setSessionData] = useState({
+    label: "Session",
+    sessionTime: 1500,
+  });
+
+  const decreaseBreakTime = () => {
+    if (breakTimeDisplay > 1) setBreakTimeDisplay((prev) => prev - 1);
+  };
+
+  const increaseBreakTime = () => {
+    if (breakTimeDisplay < 60) {
+      setBreakTimeDisplay((prev) => prev + 1);
     }
-    this.decreaseBreakTime = this.decreaseBreakTime.bind(this);
-    this.increaseBreakTime = this.increaseBreakTime.bind(this);
-    this.decreaseSessionTime = this.decreaseSessionTime.bind(this);
-    this.increaseSessionTime = this.increaseSessionTime.bind(this);
-    this.handleStartStop = this.handleStartStop.bind(this);
-    this.handleReset = this.handleReset.bind(this);
-  }
+  };
 
-  decreaseBreakTime() {
-    if (this.state.breakTimeDisplay > 1)
-      this.setState({
-        breakTimeDisplay: this.state.breakTimeDisplay - 1
-      })
-  }
-
-  increaseBreakTime() {
-    if(this.state.breakTimeDisplay < 60){
-      this.setState({
-      breakTimeDisplay: this.state.breakTimeDisplay + 1
-      })
-    }
-  }
-    
-
-  decreaseSessionTime() {
-    if (this.state.sessionTimeDisplay > 1) {
-      this.setState({
-        sessionTime: (this.state.sessionTimeDisplay - 1) * 60,
-        sessionTimeDisplay: this.state.sessionTimeDisplay - 1
-      })
-    }
-  }
-
-  increaseSessionTime() {
-    if(this.state.sessionTimeDisplay < 60){
-      this.setState({
-      sessionTime: (this.state.sessionTimeDisplay + 1) * 60,
-      sessionTimeDisplay: this.state.sessionTimeDisplay + 1
-    })}
-    
-  }
-
-  handleStartStop(){
-    if(!this.state.counting && this.state.sessionTime > 0){
-    this.setState({
-      sessionInterval: setInterval(()=>{
-      if(this.state.sessionTime > 0){
-        this.setState({
-        sessionTime: this.state.sessionTime - 1
-    })
-      } else if(this.state.timerLabel == "Session"){
-        this.setState({
-          sessionTime: this.state.breakTimeDisplay * 60,
-          timerLabel: "Break"
-        });
-        this.playSound();
-      } else if(this.state.timerLabel == "Break"){
-        this.setState({
-          sessionTime: (this.state.sessionTimeDisplay) * 60,
-          timerLabel: "Session"
-        })
-        this.playSound();
-      }
-    },1000),
-      counting: true
-    })
-    } else {
-      clearInterval(this.state.sessionInterval);
-      this.setState({
-        counting: false
+  const decreaseSessionTime = () => {
+    if (sessionTimeDisplay > 1) {
+      setSessionData((prev) => {
+        return {
+          label: prev.label,
+          sessionTime: (sessionTimeDisplay - 1) * 60,
+        };
       });
+      setSessionTimeDisplay((prev) => prev - 1);
     }
-  }
+  };
 
-  handleReset(){
-    clearInterval(this.state.sessionInterval);
-    this.setState({
-      breakTimeDisplay: 5,
+  const increaseSessionTime = () => {
+    if (sessionTimeDisplay < 60) {
+      setSessionData((prev) => {
+        return {
+          label: prev.label,
+          sessionTime: (sessionTimeDisplay + 1) * 60,
+        };
+      });
+      setSessionTimeDisplay((prev) => prev + 1);
+    }
+  };
+
+  const handleReset = () => {
+    clearInterval(counter);
+    setBreakTimeDisplay(5);
+    setSessionTimeDisplay(25);
+    setCounting(false);
+    setCounter(null);
+    setSessionData({
+      label: "Session",
       sessionTime: 1500,
-      sessionTimeDisplay: 25,
-      counting: false,
-      timerLabel: "Session"
     });
     const sound = document.getElementById("beep");
     sound.currentTime = 0;
     sound.pause();
-  }
+  };
 
-  playSound(){
+  const handleStartStop = () => {
+    if (!counting && sessionData.sessionTime > 0) {
+      setCounting(true);
+      setCounter(
+        setInterval(() => {
+          setSessionData((prev) => {
+            if (prev.sessionTime > 0) {
+              return {
+                label: prev.label,
+                sessionTime: prev.sessionTime - 1,
+              };
+            }
+            if (prev.sessionTime === 0) {
+              playSound();
+              if (prev.label === "Session") {
+                return {
+                  label: "Break",
+                  sessionTime: breakTimeDisplay * 60,
+                };
+              }
+              if (prev.label === "Break") {
+                return {
+                  label: "Session",
+                  sessionTime: sessionTimeDisplay * 60,
+                };
+              }
+            } else {
+              return prev;
+            }
+          });
+        }, 1000)
+      );
+    } else {
+      clearInterval(counter);
+      setCounting(false);
+    }
+  };
+
+  const playSound = () => {
     const sound = document.getElementById("beep");
     sound.play();
     sound.currentTime = 0;
-  }
+  };
 
-  render() {
-    return (
-      <main>
-        <h1>25 + 5 Clock</h1>
-        <BreakControl decreaseBreakTime={this.decreaseBreakTime} increaseBreakTime={this.increaseBreakTime} breakTimeDisplay={this.state.breakTimeDisplay} />
-        <SessionControl decreaseSessionTime={this.decreaseSessionTime} increaseSessionTime={this.increaseSessionTime} sessionTimeDisplay={this.state.sessionTimeDisplay} />
-        <Session timerLabel={this.state.timerLabel} sessionTime={this.state.sessionTime} sessionSeconds={this.state.sessionSeconds} />
-        <Controls handleStartStop={this.handleStartStop} handleReset={this.handleReset} />
-        <audio id="beep" src={"//raw.githubusercontent.com/freeCodeCamp/cdn/master/build/testable-projects-fcc/audio/BeepSound.wav"} />
-      </main>
-    )
-  }
+  return (
+    <main>
+      <h1>25 + 5 Clock</h1>
+      <BreakControl
+        decreaseBreakTime={decreaseBreakTime}
+        increaseBreakTime={increaseBreakTime}
+        breakTimeDisplay={breakTimeDisplay}
+      />
+      <SessionControl
+        decreaseSessionTime={decreaseSessionTime}
+        increaseSessionTime={increaseSessionTime}
+        sessionTimeDisplay={sessionTimeDisplay}
+      />
+      <Session
+        timerLabel={sessionData.label}
+        sessionTime={sessionData.sessionTime}
+      />
+      <Controls handleStartStop={handleStartStop} handleReset={handleReset} />
+      <audio
+        id="beep"
+        src={
+          "//raw.githubusercontent.com/freeCodeCamp/cdn/master/build/testable-projects-fcc/audio/BeepSound.wav"
+        }
+      />
+    </main>
+  );
 }
